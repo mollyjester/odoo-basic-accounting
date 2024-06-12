8684e3c0443dd66928e5e033e347b8ef60983776
Sergei Likharev <slikharev@carabaosolutions.com>
2024-06-12 15:42:33 +0200

    Fixed license type

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
 
     'author': "Sergei Likharev",
     'website': "",
-    'license': "GNU GPL-3",
+    'license': "GPL-3",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
