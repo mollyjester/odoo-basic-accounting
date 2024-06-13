a5b7136ac2b4015ecb4c9be43de9ad79185ed5a9
Sergei Likharev <slikharev@carabaosolutions.com>
2024-06-13 11:57:03 +0200

    Access rights

--- __manifest__.py
+++ __manifest__.py
@@ -25,8 +25,9 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml'
-
     ],
+    'installable': True,
+    'application': True
 }
